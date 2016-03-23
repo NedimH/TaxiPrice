204d83c2a8ffc6c77984b2abd50a1efd144c3b25
famela <ludakrava1!>
2016-03-23 18:17:08 +0100

    dodate karte na gradove

--- resources/views/taxi/showtaxi.blade.php
+++ resources/views/taxi/showtaxi.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
+    <div id="map" style="width: 700px; height: 300px;"></div>
+    <script>
+        function initMap() {
+            var mapDiv = document.getElementById('map');
+            var map = new google.maps.Map(mapDiv, {
+                center: {lat: {{$podaci->LAT}}, lng: {{$podaci->LON}}},
+                zoom: 8
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?callback=initMap"
+            async defer></script>
+
     <div>
         <h2>Država: {{ $podaci->COUNTRY_NAME }}</h2>
 
